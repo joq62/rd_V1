69ad5fa3655aeb8830de7ba05bbf2b6461564784
joq62 <joq62@c50>
2024-06-14 10:46:52 +0200

    created

---
+++ src/rd_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc rd public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(rd_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    rd_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
